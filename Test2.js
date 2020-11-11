5b9558c7705eb469e8e00803a0802763754c34de
Joel <joeldimeglio@gmail.com>
2020-11-10 22:38:46 -0600

    Test 2

---
+++ Test2.js
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+    alert( 'Hello, world! This is test two!' );
+  </script>
+
+  <p>...After the script.</p>
+
+</body>
+
+</html>
